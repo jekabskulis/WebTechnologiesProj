b4a72029b0459a7556a713df57af2f3e2d0942ea
jekabskulis <jekabs.kulis@svg.lv>
2024-01-16 23:04:47 +0000

    Added image information overlay.

--- src/script.js
+++ src/script.js
@@ -1,7 +1,27 @@
 
-item1 = document.getElementById("item-1");
+const item1 = document.getElementById("item-1");
+const itemOverlay = document.getElementById("item__overlay");
+const itemOverlaySelect = document.getElementById("item__overlay__info")
+
+let checkOverlaySelect = true;
 
 item1.addEventListener("click", () => 
 {
-    console.log("34etr5yh");
+    itemOverlay.style.display = "block";
+});
+
+itemOverlay.addEventListener("click", () =>
+{
+    if(checkOverlaySelect)
+    {
+        itemOverlay.style.display = "none";
+    }
+    checkOverlaySelect = true;
+});
+
+
+
+itemOverlaySelect.addEventListener("click", () =>
+{
+    checkOverlaySelect = false;
 });
